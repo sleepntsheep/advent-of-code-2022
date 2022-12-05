00b01af4fc11234a201f7440f51054e1ee822c42
sleepntsheep <contact@papangkorn.com>
2022-12-05 12:58:44 +0700

    e

--- 5.c
+++ 5.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <inttypes.h>
 #include <string.h>
 #include <assert.h>
-
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 #define NSTK 10
 #define NCRATE 10000
@@ -32,39 +26,36 @@ int main(void) {
         *p = 0;
 
         if (buf[0] == '\0') {
-        } if (buf[1] == '1') {
+            continue;
+        } else if (buf[1] == '1') {
             for (size_t c = 1; c <= NSTK; c++) {
                 s_strrev(a[c], ap[c]);
                 strcpy(a2[c], a[c]);
             }
         } else if (strchr(buf, '[')) {
             char *x = buf + 1;
-            for (size_t c = 1; x < p; c++) {
+            for (size_t c = 1; x < p; c++, x += 4) {
                 assert(c <= NSTK && "NSTK too small");
                 if (*x != ' ')
                     a[c][ap[c]++] = *x;
-                x += 4;
             }
         } else {
             int nm, from, to;
             if (sscanf(buf, " move %d from %d to %d", &nm, &from, &to) != 3)
                 continue;
             assert(ap[from] >= nm && "Moving from empty stack, wrong input?");
-            for (int i = 0; i < nm; i++) {
+            for (int i = 0; i < nm; i++)
                 a[to][ap[to]++] = a[from][--ap[from]];
-            }
             memcpy(a2[to] + ap[to] - nm, a2[from] + ap[from], nm);
         }
     }
 
-    for (size_t c = 1; c <= NSTK; c++) {
+    for (size_t c = 1; c <= NSTK; c++)
         printf("%c", a[c][ap[c]-1]);
-    }
 
     printf(" ");
-    for (size_t c = 1; c <= NSTK; c++) {
+    for (size_t c = 1; c <= NSTK; c++)
         printf("%c", a2[c][ap[c]-1]);
-    }
 
     return EXIT_SUCCESS;
 }
