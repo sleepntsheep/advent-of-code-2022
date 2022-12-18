ef229f522c7d5addca300ecc06d78d9da7e0546f
sleepntsheep <contact@papangkorn.com>
2022-12-18 08:52:45 +0700

    Feat: Day 17 Part 2

--- 17.cpp
+++ 17.cpp
@@ -1,17 +1,27 @@
 #include <cstdint>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <map>
 #include <vector>
 #include <utility>
 #include <cassert>
 
-const std::vector<uint8_t> rocks[] = {
+const std::vector<uint8_t> Rocks[] = {
     {0b11110},
     {0b01000, 0b11100, 0b01000},
     {0b11100, 0b00100, 0b00100},
     {0b10000, 0b10000, 0b10000, 0b10000},
     {0b11000, 0b11000},
+};
+
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator () (const std::pair<T1,T2> &p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+        return h1 ^ h2;  
+    }
 };
 
 class NotTetris {
@@ -20,7 +30,7 @@ class NotTetris {
         ~NotTetris() { }
 
         void round() {
-            std::vector<uint8_t> rock = rocks[rock_i++ % 5];
+            std::vector<uint8_t> rock = Rocks[rock_i++ % 5];
             int ry = v.size() + 3;
             while (1) {
                 rock = push(rock, ry);
@@ -34,7 +44,7 @@ class NotTetris {
         }
 
         uint64_t get_height() {
-            return v.size();
+            return v.size() + height_offset;
         }
 
         void print() {
@@ -46,55 +56,93 @@ class NotTetris {
             }
         }
 
-        bool collide(const std::vector<uint8_t> &rocks, int height) {
+        bool collide(const std::vector<uint8_t> &rock, int height) {
             if (height >= v.size())
                 return false;
-            for (int i = 0; i < rocks.size(); i++) {
+            for (int i = 0; i < rock.size(); i++) {
                 while (height + i >= v.size())
                     return false;
-                if (rocks[i] & v[height + i])
+                if (rock[i] & v[height + i])
                     return true;
             }
             return false;
         }
 
-        bool fall(const std::vector<uint8_t> &rocks, int height) {
-            if (height == 0 || collide(rocks, height - 1))
+        bool fall(const std::vector<uint8_t> &rock, int height) {
+            if (height == 0 || collide(rock, height - 1))
                 return false;
             return true;
         }
 
-        void rest(const std::vector<uint8_t> &rocks, int height) {
-            for (int i = 0; i < rocks.size(); i++) {
+        void rest(const std::vector<uint8_t> &rock, int height) {
+            for (int i = 0; i < rock.size(); i++) {
                 if (height + i >= v.size()) {
-                    v.push_back(rocks[i]);
+                    v.push_back(rock[i]);
                 } else {
-                    v[height + i] |= rocks[i];
+                    v[height + i] |= rock[i];
                 }
             }
         }
 
-        std::vector<uint8_t> push(const std::vector<uint8_t> &rocks, int height) {
+        std::vector<uint8_t> push(const std::vector<uint8_t> &rock, int height) {
             std::vector<uint8_t> moved;
             char direction = jet[jet_pos++];
             jet_pos %= jet.size();
-            for (int i = 0; i < rocks.size(); i++) {
+            for (int i = 0; i < rock.size(); i++) {
                 if (direction == '<') {
-                    if (!(rocks[i] & 0b1000000))
-                        moved.push_back(rocks[i] << 1);
+                    if (!(rock[i] & 0b1000000))
+                        moved.push_back(rock[i] << 1);
                 } else {
-                    if (!(rocks[i] & 0b0000001))
-                        moved.push_back(rocks[i] >> 1);
+                    if (!(rock[i] & 0b0000001))
+                        moved.push_back(rock[i] >> 1);
                 }
             }
-            if (moved.size() != rocks.size() || collide(moved, height)) {
-                return rocks;
+            if (moved.size() != rock.size() || collide(moved, height)) {
+                return rock;
             }
             return moved;
         }
 
+        uint64_t fast_simulate(uint64_t nrnd) {
+            uint64_t crnd = 0;
+
+            using pu64 = std::pair<uint64_t, uint64_t>;
+            std::map<pu64, pu64> mp;
+
+            for (;crnd < nrnd;) {
+                round();
+                crnd++;
+
+                if (mp.count({crnd % 5, jet_pos})) {
+                    bool true_cycle = true;
+                    auto [height, start] = mp[{crnd % 5, jet_pos}];
+
+                    constexpr int N_CHECK_ROW = 16;
+                    for (size_t i = 1; i <= N_CHECK_ROW; i++) {
+                        if (v[height - i] != v[v.size() - i]) {
+                            true_cycle = false;
+                            break;
+                        }
+                    }
+
+                    auto cycle_rnd = crnd - start;
+                    if (true_cycle && cycle_rnd) {
+                        auto cycle_height = get_height() - height;
+                        auto iters = (nrnd - crnd) / cycle_rnd;
+                        height_offset += iters * cycle_height;
+                        crnd += iters * cycle_rnd;
+                    }
+                } else {
+                    mp[{crnd % 5, jet_pos}] = {get_height(), crnd};
+                }
+            }
+
+            return get_height();
+        }
+
         std::vector<uint8_t> v;
         size_t jet_pos = 0;
+        uint64_t height_offset = 0;
         int rock_i = 0;
         std::string jet;
     private:
@@ -113,15 +161,9 @@ int main()
     }
 
     { // PART 2 NOT DONE
-        uint64_t nrnd = 1e12;
-        uint64_t crnd = 0;
-        uint64_t height_offset = 0;
-
-        using pu64 = std::pair<uint64_t, uint64_t>;
-        std::map<pu64, pu64> mp;
         NotTetris nt(jet);
-
-        std::cout << height_offset + nt.get_height() << std::endl;
+        nt.fast_simulate(1000000000000LL);
+        std::cout << nt.get_height() << std::endl;
     }
 
     return EXIT_SUCCESS;
